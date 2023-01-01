2dffc940190a4837faf23b310d9adc833d66e007
Tahsin-Zaman-Jilan <77837621+Tahsin-Zaman-Jilan@users.noreply.github.com>
2023-01-02 00:11:17 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello world</h1>
+    <p>I am back</p>
+  </div>,
+
+  document.getElementById("root")
+);
